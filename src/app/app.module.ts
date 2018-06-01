808e0a602adbfbd0027691557bdda8c3a8c655fe
isradeleon <isra.backend@gmai.com>
2018-05-31 21:03:03 -0500

    new routing folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule, MyComponents } from './app-routing.module';
+import { AppRoutingModule, MyComponents } from './routing/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { FeedComponent } from './components/feed/feed.component';
-import { PostComponent } from './components/post/post.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { UserComponent } from './components/user/user.component';
-import { LoginComponent } from './components/login/login.component';
-import { AuthService } from './services/auth.service';
-import { PreventloginService } from './services/preventlogin.service';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { GalleryComponent } from './components/gallery/gallery.component'
-import { TodosComponent } from './components/todos/todos.component'
-import { PhotosComponent } from './components/photos/photos.component'
+import { AppComponent } from '../app.component';
+import { FeedComponent } from '../components/feed/feed.component';
+import { PostComponent } from '../components/post/post.component';
+import { CommentsComponent } from '../components/comments/comments.component';
+import { UserComponent } from '../components/user/user.component';
+import { LoginComponent } from '../components/login/login.component';
+import { AuthService } from '../services/auth.service';
+import { PreventloginService } from '../services/preventlogin.service';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { GalleryComponent } from '../components/gallery/gallery.component'
+import { TodosComponent } from '../components/todos/todos.component'
+import { PhotosComponent } from '../components/photos/photos.component'
 
 const routes: Routes = [
   { path: '', component: FeedComponent, canActivate:[AuthService] },
